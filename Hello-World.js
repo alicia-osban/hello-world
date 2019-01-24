ac5ffa83cd766db490350b05e237602374043712
Alicia <alicia.osban@gmail.com>
2019-01-24 11:13:11 -0500

    switch to express application

--- Hello-World.js
+++ Hello-World.js
@@ -1,2 +1,13 @@
-console.log("Hello World");
+const http = require("http");
+const pkg = require("./package.json");
+const app = require("express")(); 
+const { join } = require("path"); 
 
+app.get("/", function(req, res){
+  console.log("incoming request", req.url);
+  const path = join(__dirname, "dist", "index.html");
+  console.log("index.html path", path);
+  res.sendFile(path);
+});
+
+http.createServer(app).listen(3000);
